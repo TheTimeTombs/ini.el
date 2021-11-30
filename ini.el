8561301f859f74923a5c88b115cbb5c350b578e0
Pierre Rouleau <prouleau001@gmail.com>
2021-11-30 16:21:05 -0500

    Improve code: returned alist is in order of text, support # comments, really skip empty lines, reduce forms.

--- ini.el
+++ ini.el
@@ -20,39 +20,40 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
-(defun ini-decode (ini-text)
-  "Convert INI-TEXT into a lisp alist object."
+(defun ini-decode (ini-text &optional comment-regexp)
+  "Convert INI-TEXT into a lisp alist object.
+
+By default treats a semicolon at the beginning of the line as a comment
+unless something else is specified in the COMMENT-REGEXP."
   (if (not (stringp ini-text))
       (error "Must be a string"))
   (let ((lines (split-string ini-text "\n"))
 	(section)
 	(section-list)
-	(alist))
+	(alist)
+        (cmt-regexp (or comment-regexp "^;")))
     (dolist (line lines)
       ;; skip comments
-      (unless (or (string-match "^;" line)
-		  (string-match "^[ \t]$" line))
+      (unless (or (string-match cmt-regexp line)
+		  (string-match "^[ \t]*$" line))
 	;; catch sections
-	(if (string-match "^\\[\\(.*\\)\\]$" line)
-	    (progn
-	      (if section
-		  ;; add as sub-list
-		  (setq alist (cons `(,section . ,section-list) alist))
-		(setq alist section-list))
-	      (setq section (match-string 1 line))
-	      (setq section-list nil)))
+	(when (string-match "^\\[\\(.*\\)\\]$" line)
+          (if section
+              ;; add as sub-list
+              (setq alist (cons `(,section . ,section-list) alist))
+            (setq alist section-list))
+          (setq section (match-string 1 line))
+          (setq section-list nil))
         ;; catch properties
 	(if (string-match "^\\([^\s\t]+\\)[\s\t]*=[\s\t]*\\(.+\\)$" line)
 	    (let ((property (match-string 1 line))
 		  (value (match-string 2 line)))
-	      (progn
-		(setq section-list (cons `(,property . ,value) section-list)))))))
+	      (setq section-list (cons `(,property . ,value) section-list))))))
     (if section
 	;; add as sub-list
 	(setq alist (cons `(,section . ,section-list) alist))
       (setq alist section-list))
-    alist))
-
+    (reverse alist)))
 
 (defun ini-encode (ini-alist)
   "Convert a INI-ALIST into .INI formatted text."
