5561680ce2fa30bb21ad8771b1b63109cfcb4d33
Pierre Rouleau <prouleau001@gmail.com>
2021-11-30 14:55:45 -0500

    Cosmetic: rename the variable 'l' to 'line'
    - The code places the letter 'l' beside the number '1'.  These look very similar in many fonts.
    - Using single and plural is used in several function languages. That's what I used instead.

--- ini.el
+++ ini.el
@@ -20,7 +20,7 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
-(defun ini-decode (ini_text) 
+(defun ini-decode (ini_text)
   ;; text -> alist
   (interactive)
   (if (not (stringp ini_text))
@@ -29,24 +29,24 @@
 	(section)
 	(section-list)
 	(alist))
-    (dolist (l lines)
+    (dolist (line lines)
       ;; skip comments
-      (unless (or (string-match "^;" l)
-		  (string-match "^[ \t]$" l))
+      (unless (or (string-match "^;" line)
+		  (string-match "^[ \t]$" line))
 	;; catch sections
-	(if (string-match "^\\[\\(.*\\)\\]$" l)
-	    (progn 
+	(if (string-match "^\\[\\(.*\\)\\]$" line)
+	    (progn
 	      (if section
 		  ;; add as sub-list
 		  (setq alist (cons `(,section . ,section-list) alist))
 		(setq alist section-list))
-	      (setq section (match-string 1 l))
+	      (setq section (match-string 1 line))
 	      (setq section-list nil)))
 	      ;; catch properties
-	      (if (string-match "^\\([^\s\t]+\\)[\s\t]*=[\s\t]*\\(.+\\)$" l)
-		  (let ((property (match-string 1 l))
-			(value (match-string 2 l)))
-		    (progn 
+	      (if (string-match "^\\([^\s\t]+\\)[\s\t]*=[\s\t]*\\(.+\\)$" line)
+		  (let ((property (match-string 1 line))
+			(value (match-string 2 line)))
+		    (progn
 		      (setq section-list (cons `(,property . ,value) section-list)))))))
     (if section
 	;; add as sub-list
@@ -67,11 +67,11 @@
 	(when (not (stringp key))
 	  (error "key is not a string"))
 	(if (listp value)
-	    (setq txt 
-		  (concat txt 
+	    (setq txt
+		  (concat txt
 			  (format "[%s]\n" key)
 			  (ini-encode value)))
-	  (setq txt 
+	  (setq txt
 		(concat txt (format "%s=%s\n" key value))))))
     txt))
 
